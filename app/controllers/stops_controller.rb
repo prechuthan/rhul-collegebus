08b078c41bfa64c4504efe917b297bf67f37b463
Preshant Achuthan <engerium@gmail.com>
2020-02-04 15:46:16 +0000

    Renamed stops controller index action to show action

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,4 +1,4 @@
 class StopsController < ApplicationController
-  def index
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  match '/dashboard', to: "dashboard#index", via: "get"
-
-  match '/stops', to: "stops#index", via: "get"
 
   root 'welcome#index'
+
+  resource :stops, only: [:show]
+
+  match '/dashboard', to: "dashboard#index", via: "get"
+    #match '/stops', to: "stops#index", via: "get"
+
+
 end
