ddd2668c9f7ec3e1f150ffe65bb63aeb0ec13a42
Preshant Achuthan <engerium@gmail.com>
2020-02-07 15:22:35 +0000

    Added new and create actions for dashboard timetables controller

--- app/controllers/dashboard/timetables_controller.rb
+++ app/controllers/dashboard/timetables_controller.rb
@@ -4,4 +4,18 @@ class Dashboard::TimetablesController < ApplicationController
     @timetables = Timetable.all
   end
 
+  def new
+    @timetable = Timetable.new
+  end
+
+  def create
+    @timetable = Timetable.new(timetable_params)
+  end
+
+  private
+  def timetable_params
+    params.require(:bus).permit(:bus_id, :departure_time, :departure_stop_id, :arrival_time, :arrival_stop_id,
+                                :is_operational_weekday, :is_operational_weekend, :is_operational_term,
+                                :is_operational_winter, :is_operational_easter, :is_operational_summer,)
+  end
 end
