70712fafb6cd782eaffc5dd9ff844043ee3c5481
Preshant Achuthan <engerium@gmail.com>
2020-02-07 13:35:53 +0000

    Updated dashbaord bus controller to redirect back after save

--- app/controllers/dashboard/buses_controller.rb
+++ app/controllers/dashboard/buses_controller.rb
@@ -9,4 +9,18 @@ class Dashboard::BusesController < ApplicationController
     @bus = Bus.new
   end
 
+  def create
+    @bus = Bus.new(bus_params)
+
+    if @bus.save
+      redirect_to dashboard_buses_path
+    else
+      render `new`
+    end
+  end
+
+  private
+  def bus_params
+    params.require(:bus).permit(:service_number)
+  end
 end
